7e62bd45383cc53ea5bc44e50a3453e86a690d94
rajgupta2399 <rajumardotnet2499@gmail.com>
2024-03-20 18:01:00 +0530

    asdf

--- js/global.js
+++ js/global.js
@@ -2,20 +2,3 @@
 //     el: document.querySelector('#main'),
 //     smooth: true,
 // });
-
-// add event on multiple element 
-
-const addEventOnElements = function (elements,eventType,callback){
-    for(const elem of elements) elem.addEventListener(eventType,callback);
-}
-
-// toggle search box in mob device 
-
-const searchBox = document.querySelector("[search-box]");
-const searchTogglers = document.querySelectorAll("[search-toggler]");
-
-addEventOnElements(searchTogglers,"click",function(){
-    searchBox.classList.toggle("active");
-})
-
-
