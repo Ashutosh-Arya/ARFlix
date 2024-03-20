429a9777f6a37cf31c9f370c2a921a83c54f8fb9
rajgupta2399 <rajumardotnet2499@gmail.com>
2024-03-20 23:24:55 +0530

    update

--- js/global.js
+++ js/global.js
@@ -2,3 +2,18 @@
 //     el: document.querySelector('#main'),
 //     smooth: true,
 // });
+
+
+
+const addEventOnElements = function (elements,eventType,callback){
+    for(const elem of elements) elem.addEventListener(eventType,callback);
+}
+
+// toggle search box in mob device 
+
+const searchBox = document.querySelector(".search-box");
+const searchTogglers = document.querySelectorAll(".search-toggler");
+
+addEventOnElements(searchTogglers,"click",function(){
+    searchBox.classList.toggle("active");
+})
