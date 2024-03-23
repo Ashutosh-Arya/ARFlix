1b3fc4907ed9af57098c864f2f1e43eced9cf0db
rajgupta2399 <rajumardotnet2499@gmail.com>
2024-03-23 10:01:38 +0530

    banner

--- js/global.js
+++ js/global.js
@@ -1,19 +1,56 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),
-//     smooth: true,
-// });
+// ------------------------------- HEADER -------------------------
+const searchBox = document.querySelector(".search-box");
+const searchToggler = document.querySelectorAll(".search-toggler");
 
-
-
-const addEventOnElements = function (elements,eventType,callback){
-    for(const elem of elements) elem.addEventListener(eventType,callback);
+for (let i = 0; i < searchToggler.length; i++) {
+  searchToggler[i].addEventListener("click", () => {
+    searchBox.classList.toggle("active");
+  });
 }
 
-// toggle search box in mob device 
+// ----------------------- SIDEBAR ------------------------------
+const menuBtn = document.querySelector(".menu-btn");
+const sidebar = document.querySelector(".sidebar");
+const overlay = document.querySelector(".overlay");
+const body = document.body;
 
-const searchBox = document.querySelector(".search-box");
-const searchTogglers = document.querySelectorAll(".search-toggler");
+menuBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
 
-addEventOnElements(searchTogglers,"click",function(){
-    searchBox.classList.toggle("active");
-})
+// --------------------------- BANNER VIDEO TRAILER ---------------------------
+function toggleVideo() {
+  const trailer = document.querySelector(".trailer");
+  const video = document.querySelector("video");
+  video.pause();
+  trailer.classList.toggle("active");
+}
+
+// ----------------------------------- BANNER IMAGE ------------------------------
+function changeBg(bg, title) {
+  const banner = document.querySelector(".banner");
+  const contents = document.querySelectorAll(".content");
+  banner.style.background = `url("../Img/${bg}")`;
+  banner.style.backgroundSize = "cover";
+  banner.style.backgroundPosition = "center";
+  banner.style.backgroundRepeat = "no-repeat";
+
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+    if(content.classList.contains(title)) {
+      content.classList.add("active");
+    }
+  });
+}
+
+
+// -------------------------------- CAROUSEL ---------------------------------------------
+
+$(document).ready(function () {
+  $(".carousel").carousel();
+});
+
+// ------------------------------------- END -----------------------------------------------
